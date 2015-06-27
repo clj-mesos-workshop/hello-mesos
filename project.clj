17953593f432c021c93f65a3046007def77b6be2
Prasanna Gautam <prasannagautam@gmail.com>
2015-06-27 12:52:16 -0700

    Adding Gorilla-Repl and start-repl script

--- project.clj
+++ project.clj
@@ -1,12 +1,16 @@
 (defproject hello-mesos "0.1.0-SNAPSHOT"
-  :description "TODO"
-  :url "TODO"
+  :description "Introduction to Mesos Framework using Clojure"
+  :url "https://github.com/clj-mesos-workshop/hello-mesos"
   :license {:name "MIT"
             :url "https://github.com/edpaget/hello-mesos/LICENSE"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [bdisraeli/clj-mesos "0.20.9-SNAPSHOT"]
                  [com.stuartsierra/component "0.2.2"]
                  [leiningen "2.5.1"]]
+  :main ^:skip-aot gorilla-test.core
+  :target-path "target/%s"
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.7"]]
                    :source-paths ["dev"]}
-             :user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]]}})
+             :user {:plugins [[cider/cider-nrepl "0.9.1"]
+                              [lein-gorilla "0.3.4"]]}
+             :uberjar {:aot :all}})
---
+++ start-repl.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+lein gorilla :ip 0.0.0.0 :nrepl-port 8090 :port 8091
