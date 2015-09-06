a22755dce32c4ea290c889542063447b30f038b5
Prasanna Gautam <prasannagautam@gmail.com>
2015-09-06 16:05:50 -0700

    Adding timestamp to be able to easily tell it's running

--- src/hello_mesos/executor.clj
+++ src/hello_mesos/executor.clj
@@ -1,12 +1,13 @@
 (ns hello-mesos.executor
-  (:require [clj-mesos.executor :as mesos]))
+  (:require [clj-mesos.executor :as mesos])
+  (:import [java.util Date]))
 
 (defn executor
   []
   (mesos/executor
    (launchTask [driver task-info]
                (future (loop []
-                         (println "Hey Mesos!")
+                         (println (Date.) "Hey Mesos!")
                          (Thread/sleep 2000)
                          (recur)))
                (mesos/send-status-update driver {:task-id (:task-id task-info)
