82b217dfcf27be386b42b942295f69b0d19f265f
Edward Paget <edward@cassetteta.pe>
2015-09-18 09:58:36 -0500

    Create basic framework from mesos-framework template

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject hello-mesos "0.1.0-SNAPSHOT"
+  :description "Lein template for Apache Mesos Frameworks"
+  :url "TODO: ADD LINK TO WEBSITE"
+  :license {:name "TODO: CHOOSE A LICENSE"
+            :url "TODO: LINK TO IT" }
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-mesos "0.22.0"]
+                 [com.stuartsierra/component "0.2.3"]]
+  :target-path "target/%s"
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.7"]
+                                  [alembic "0.3.2"]]
+                   :source-paths ["dev"]}
+             :uberjar {:aot :all}})
